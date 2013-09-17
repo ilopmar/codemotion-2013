80b43b94bc5c4da1eb277f1f9dc542b421dfaccc
Iván López <lopez.ivan@gmail.com>
2013-09-17 23:44:24 +0200

    Flight demo. Add queue to send emails asynchronously

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,6 +3,8 @@ beans = {
     xmlns si:   "http://www.springframework.org/schema/integration"
 
     // Flight
+    si.poller(default:"true", "fixed-rate":"1000")
+
     si.channel(id: "bookingConfirmationRequests")
     si."service-activator"("input-channel": "bookingConfirmationRequests",
                            "output-channel": "chargedBookings",
@@ -14,7 +16,9 @@ beans = {
                             ref: "seatAvailabilityService",
                             method: "confirmSeat")
 
-    si.channel(id: "emailConfirmationRequests")
+    si.channel(id: "emailConfirmationRequests") {
+        queue(capacity: 2)
+    }
     si."outbound-channel-adapter"(channel: "emailConfirmationRequests",
                                   ref: "emailConfirmationService")
 
